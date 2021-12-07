557593d1d581db46ceb0c670a83b46a320dd20dd
Danny Putnam <dannykputnam@gmail.com>
2021-12-06 21:02:17 -0700

    account and user from dec 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'accounts#index'
+
+  devise_for :users
+
+  resources :accounts do 
+      resources :items
+  end
+
+
+  # Parent child example
+  # resources :items do
+  #   resources :comments
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
